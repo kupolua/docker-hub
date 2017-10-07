aa993de56cc56c939971b63187dbde1b9e86a1db
Pavel Kulakovsky <pkulakovsky@gmail.com>
2017-10-07 18:30:25 +0300

    nodejs and npm onboard

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -1,32 +1,5 @@
 #!/bin/sh
 
-function getGitSource {
-    git fetch
-    git pull origin ${GIT_BRANCH}
-}
+cd ${WEBUI_PATH}
 
-git init
-git remote add origin -f ${GIT_REPO_URL}
-
-if [ ${WEBUI_PATH} ]
-then
-    git config core.sparseCheckout true
-    echo ${WEBUI_PATH} >> .git/info/sparse-checkout
-fi
-
-git pull origin ${GIT_BRANCH}
-
-git checkout ${GIT_BRANCH}
-
-getGitSource
-
-if [ ${GIT_REPO_SYNC_TIME} ]
-then
-    i=1
-    while [ ${i} ];
-    do
-        getGitSource
-
-        sleep ${GIT_REPO_SYNC_TIME}
-    done
-fi
+npm start
